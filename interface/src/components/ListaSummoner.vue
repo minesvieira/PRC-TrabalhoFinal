dc80c4457e8413fcc3af050bf3ecf478758ae884
TartesDaJulia <henriquejlcsr@gmail.com>
2020-07-02 21:27:28 +0100

    Summoner dialog pop up

--- interface/src/components/ListaSummoner.vue
+++ interface/src/components/ListaSummoner.vue
@@ -1,23 +1,51 @@
 <template>
   <v-container class="black">
     <v-row no-gutters>
-      <template v-for="item in summoner">
-        <v-col :key="item.name">
-          <v-card
+      <template v-for="(item,index) in summoner">
+        <v-col :key="index">
+          <v-dialog
+            v-model="dialog"
+            width="500"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <div class='activator' v-bind="attrs"
+                v-on="on">
+              <v-img v-bind:src="require('@/assets/spell/' + item.path)" aspect-ratio="1" height="80" width="80"/>
+             <v-card-title primary-title> {{ item.name }} </v-card-title>
+              </div>
+            </template>
+
+            <v-card>
+              <v-card
             class="black"
             outlined
             tile
             justify="left"
+            center 
           >
-             <v-img v-bind:src="require('@/assets/spell/' + item.path)" aspect-ratio="1" height="80" width="80"> </v-img>
-             <div class="white--text" justify="center" align="center"> {{ item.name }} </div>
-             
+             <v-img center v-bind:src="require('@/assets/spell/' + item.path)" aspect-ratio="1" height="80" width="80"/>
+             <v-card-title primary-title> {{ item.name }} </v-card-title>
+             <v-card-text>
+                {{item.desc}}
+              </v-card-text>
           </v-card>
-        </v-col>
+              <v-divider></v-divider>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-dialog></v-col>
+           <v-responsive
+          v-if="index === 6"
+          :key="`width-${index}`"
+          width="100%"
+        ></v-responsive>
       </template>
     </v-row>
   </v-container>
 </template>
+
 
 <script>
 import axios from 'axios'
@@ -29,7 +57,6 @@ export default {
   name: 'ListaSummoner',
 
   data: () => ({
-
     summoner: [],
     verPersonagem: mdiMovieOpen
   }),
